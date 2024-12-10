8d683602997d6c1f56762080dc449d7c5e53d8bd
eduardorittner <eduardorittnerc@gmail.com>
2024-12-10 01:40:37 -0300

    Added check for treesitter-parser in --health

--- helix-term/src/health.rs
+++ helix-term/src/health.rs
@@ -300,6 +300,8 @@ pub fn language(lang_str: String) -> std::io::Result<()> {
         lang.debugger.as_ref().map(|dap| dap.command.to_string()),
     )?;
 
+    probe_parser(lang.grammar.as_ref().unwrap_or(&lang.language_id))?;
+
     probe_protocol(
         "formatter",
         lang.formatter
